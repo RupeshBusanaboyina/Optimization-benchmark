484f1c3146aa05b58fd08dc3ac013b1432c16bd5
Rupesh Busanaboyina <rupesh.09319@gmail.com>
2019-05-04 12:07:43 +0200

    added optimizers.py

---
+++ optimizers.py
@@ -0,0 +1,14 @@
+"""This is a module containing optimizers."""
+
+
+import numpy as np
+
+class StupidGradientDescent:
+
+	def __init__(self ,alpha , x0 . y0 , fgrad):
+
+		self.state = np.array([x0 , y0])
+		self.alpha = alpha
+		self 
+
+
